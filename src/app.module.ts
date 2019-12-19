233d32731284d003498bb4b046c244095a715401
Christian Benseler <christian.benseler@bv.com.br>
2019-12-19 12:05:30 -0300

    feat: sII create task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-//import { AppController } from './app.controller';
-//import { AppService } from './app.service';
+
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [],
   providers: [],
 })
