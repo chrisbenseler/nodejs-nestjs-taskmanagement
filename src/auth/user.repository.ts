3eb3f98472d10c84ee44dd0a49e774593c59fab5
Christian Benseler <christian.benseler@bv.com.br>
2019-12-24 08:44:56 -0300

    feat: sV validation auth

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -12,6 +13,13 @@ export class UserRepository extends Repository<User> {
         const user = new User();
         user.username = username;
         user.password = password;
-        await user.save();
+        try {
+            await user.save();
+        } catch(error) {
+            if(error.code == 23505)
+                throw new ConflictException('Username already exists')
+            else
+                throw new InternalServerErrorException();
+        }
     }
 }
