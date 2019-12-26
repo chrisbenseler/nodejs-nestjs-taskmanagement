e319f822bc7aae1cef32b82a1ebc21530685a891
Christian Benseler <christian.benseler@bv.com.br>
2019-12-26 14:27:05 -0300

    sVI: jwt set up

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from'@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,7 +10,16 @@ import { UserRepository } from './user.repository';
   imports: [
     TypeOrmModule.forFeature([
       UserRepository
-    ])
+    ]),
+    JwtModule.register({
+      secret: 'somesecret',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService]
